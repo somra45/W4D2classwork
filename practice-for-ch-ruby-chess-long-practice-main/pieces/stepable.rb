da5f699d62794b0466024412ddd13d870fe4bc5a
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-03-29 13:36:16 -0400

    finish steppable

--- practice-for-ch-ruby-chess-long-practice-main/pieces/stepable.rb
+++ practice-for-ch-ruby-chess-long-practice-main/pieces/stepable.rb
@@ -7,8 +7,11 @@ module Stepable
        self.move_diffs.each do |move|
             possible_moves << [(x + move[0]) , (y + move[1])]
        end
-       possible_moves.select! {|move| board.is_valid?(move) }
-       
+       possible_moves.select! {|move| board.is_valid?(move)}
+       possible_moves.select! do |move|
+        board[move] == NullPiece || board[move].color != self.color 
+       end
+       possible_moves  
     end
 
     def move_diffs
